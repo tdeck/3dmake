f82ee1f737ea4be671ea0360d2e8aca1056bccee
Troy H. Deck <troy.deque@gmail.com>
2025-02-02 15:16:05 -0800

    First pass at an Action class
    A lot of how it's used isn't implemented yet but the annotations seem to
    work the way I'd like.

---
+++ actions/__init__.py
@@ -0,0 +1,116 @@
+import sys
+import subprocess
+
+from typing import List, TextIO, Callable, Any, Dict, Optional
+from dataclasses import dataclass, field
+from pathlib import Path
+
+from utils.indent_stream import IndentStream
+
+@dataclass(kw_only=True)
+class Context:
+    options: Any # TODO
+
+ActionName = str
+ActionFunc = Callable[[Context, TextIO, TextIO], None] # stdout, verbose stdout
+
+@dataclass(kw_only=True)
+class Action:
+    name: ActionName  # Must be unique
+
+    internal: bool = False
+    isolated: bool  # True means the verb can't be run with other verbs
+    needs_options: bool
+    takes_input_file: bool
+    implied_actions: List[ActionName] = field(default_factory=list)
+    impl: ActionFunc
+
+    def __call__(self, context: Context):
+        debug_mode = self.takes_options and context.options.debug
+
+        if self.isolated:
+            # Isolated commands don't run in a pipeline so we don't indent their output
+            return self.impl(context, sys.stdout, sys.stdout if debug_mode else subprocess.DEVNULL)
+        else:
+            indent_stream = IndentStream(sys.stdout)
+            return self.impl(context, indent_stream, indent_stream if debug_mode else subprocess.DEVNULL)
+
+# This is populated whenever an action is declared with one of the action decorators
+_action_registry: Dict[ActionName, Action] = {}
+
+#
+# Decorators
+#
+def _action_name(fn: Callable[..., Any]) -> str:
+    return fn.__name__.replace('_', '-')
+
+def _register_action(action: Action) -> Action:
+    assert action.name not in _action_registry
+    _action_registry[action.name] = action
+    return action # For the caller's convenience
+
+def isolated_action(func: Optional[ActionFunc] = None, needs_options:bool = False):
+    def wrap(func: ActionFunc) -> ActionFunc:
+        return _register_action(Action(
+            name=_action_name(func),
+            isolated=True,
+            takes_input_file=False,
+            needs_options=needs_options,
+            impl=func,
+        ))
+
+    if callable(func):
+        return wrap(func)
+    else:
+        return wrap
+
+def pipeline_action(
+    func: Optional[ActionFunc] = None,
+    implied_actions: List[Action] = [],
+    internal = False,  # Note: It would be better to call @internal_action for internal actions!
+):
+    # This is a little convenience thing we do so that the caller of pipeline_actions
+    # needs to specify actions that actually exist, and we save them from calling .name
+    implied_action_names = [a.name for a in implied_actions]
+
+    def wrap(func: ActionFunc) -> ActionFunc:
+        return _register_action(Action(
+            name=_action_name(func),
+            isolated=False,
+            takes_input_file=True,
+            needs_options=True,
+            internal=internal,
+            implied_actions=implied_action_names,
+            impl=func,
+        ))
+
+    if callable(func):
+        return wrap(func)
+    else:
+        return wrap
+
+def internal_action(
+    func: Optional[ActionFunc] = None,
+    implied_actions: List[Action] = [],
+):
+    return pipeline_action(func=func, implied_actions=implied_actions, internal=True)
+
+
+@isolated_action
+def test_no_opts():
+    print("hello world")
+
+@isolated_action(needs_options=True)
+def test_one_opt():
+    print("hello world")
+
+@pipeline_action(implied_actions=[test_one_opt])
+def test_pipeline():
+    print("hello world")
+
+@internal_action
+def some_internal():
+    print("hello world")
+
+import pprint
+pprint.pprint(_action_registry)
