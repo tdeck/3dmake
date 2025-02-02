74087d30b8889c487e8b2e1e13bda5fc8480c193
Troy Deck <troy@visitorlabs.com>
2025-02-02 01:33:32 -0800

    Improve win build script

--- scripts/windows_build.ps1
+++ scripts/windows_build.ps1
@@ -1,4 +1,6 @@
-pyinstaller 3dm.spec -y
+$ErrorActionPreference = "Stop"
+pipenv install # Often I just booted Windows and haven't updated this
+pipenv run pyinstaller 3dm.spec -y
 pushd dist
 compress-archive -Force 3dmake\* 3dmake_windows.zip
 popd
