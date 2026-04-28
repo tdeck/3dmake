966bed443198ddddc82a9ec740123f9433bb5fd5
Troy Deck <troy@visitorlabs.com>
2026-04-28 21:01:02 +0900

    Faster ZIP in Windows build script
    Time/size comparison
    
    compress-archive: 93 seconds size 270799243
    .NET optimal: 58 seconds size 270790653
    .NET fastest: 43 seconds size 296456789
    
    Since fastest makes the archive about 26MB larger, I decided to go with
    optimal which still saves 30 seconds of wait time on my laptop. Wish I
    had something faster.

--- scripts/windows_build.ps1
+++ scripts/windows_build.ps1
@@ -7,7 +7,14 @@ if ($args[0] -eq "--release") {
 }
 
 pipenv run pyinstaller 3dm.spec -y
-pushd dist
-compress-archive -Force 3dmake\* 3dmake_windows.zip
-popd
+Add-Type -Assembly System.IO.Compression.FileSystem
+$distDir = Resolve-Path 'dist'
+Remove-Item -Force "$distDir\3dmake_windows.zip" -ErrorAction SilentlyContinue
+echo 'Creating ZIP file...'
+[System.IO.Compression.ZipFile]::CreateFromDirectory(
+    "$distDir\3dmake",
+    "$distDir\3dmake_windows.zip",
+    [System.IO.Compression.CompressionLevel]::Optimal,
+    $false # includeBaseDirectory
+)
 # IMPORTANT: for the windows build the 3dm.exe is in the top level dir. This is required for self-install to work.
